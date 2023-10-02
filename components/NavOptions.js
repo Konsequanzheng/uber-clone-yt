5e025807c31f71ffb1bff12031d29c717c99687f
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-10-02 14:58:05 +0300

    Add stack navigation to NavOptions

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,6 +2,7 @@ import { Text, View, FlatList, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import { Icon } from 'react-native-elements'
+import { useNavigation } from '@react-navigation/native'
 
 const data = [
     {
@@ -19,33 +20,36 @@ const data = [
 ]
 
 const NavOptions = () => {
-  return (
-    <FlatList 
-        data={data}
-        horizontal
-        keyExtractor={(item) => item.id}
-        renderItem={({item}) => (
-            <TouchableOpacity
-                style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
-            >
-                <View>
-                    <Image
-                        source={{uri: item.image}}
-                        style={{width: 120, height: 120, resizeMode: 'contain'}}
+    const navigation = useNavigation();
+
+    return (
+        <FlatList 
+            data={data}
+            horizontal
+            keyExtractor={(item) => item.id}
+            renderItem={({item}) => (
+                <TouchableOpacity
+                    onPress={() => navigation.navigate(item.screen)}
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                >
+                    <View>
+                        <Image
+                            source={{uri: item.image}}
+                            style={{width: 120, height: 120, resizeMode: 'contain'}}
+                        />
+                        <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                    </View>
+                    <Icon 
+                        style={tw`p-2 bg-black rounded-full w-10`}
+                        name='arrowright' 
+                        color='white' 
+                        type='antdesign'
                     />
-                    <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
-                </View>
-                <Icon 
-                    style={tw`p-2 bg-black rounded-full w-10`}
-                    name='arrowright' 
-                    color='white' 
-                    type='antdesign'
-                />
-            </TouchableOpacity>
-        )}
-    />
-   
-  )
+                </TouchableOpacity>
+            )}
+        />
+    
+    )
 }
 
 export default NavOptions
