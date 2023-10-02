60a53e5fe40716a7a7ccfbe47cc0defd2074ceb7
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-10-02 15:12:02 +0300

    Fix google-places-autocomplete import

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,10 @@ import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import React from 'react'
 import tw from 'twrnc'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
 
-const HomeScreen = () => {
+const HomeScreen = (navigation) => {
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -17,6 +19,13 @@ const HomeScreen = () => {
                 uri: 'https://links.papareact.com/gzs'
             }}
         />
+
+        <GooglePlacesAutocomplete 
+          placeholder= 'Where From?'
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+        />
+
         <NavOptions />
       </View>
     </SafeAreaView>
