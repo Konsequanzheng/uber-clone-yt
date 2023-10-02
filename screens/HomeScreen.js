148c96457f30870d93793b0610390f6cfc2d08c5
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-10-02 15:27:56 +0300

    Implement places search box

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,8 +4,12 @@ import tw from 'twrnc'
 import NavOptions from '../components/NavOptions'
 import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
 import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
 
 const HomeScreen = (navigation) => {
+  const dispatch = useDispatch();
+
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -24,6 +28,32 @@ const HomeScreen = (navigation) => {
           placeholder= 'Where From?'
           nearbyPlacesAPI='GooglePlacesSearch'
           debounce={400}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
+
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={'search'}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
         />
 
         <NavOptions />
