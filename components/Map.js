461d67556e505edc199e2afb1e18af997e22473a
Quan Zheng <q.zheng-7@student.tudelft.nl>
2023-10-03 19:57:54 +0300

    Change maps API language to english

--- components/Map.js
+++ components/Map.js
@@ -30,7 +30,7 @@ const Map = () => {
         if(!origin || !destination) return;
 
         const getTravelTime = async() => {
-            fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}`)
+            fetch(`https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=${origin.description}&destinations=${destination.description}&key=${GOOGLE_MAPS_APIKEY}&language=en`)
                 .then((res) => res.json())
                 .then(data => {
                     dispatch(setTravelTimeInformation(data.rows[0].elements[0]))                
